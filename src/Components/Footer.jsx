2bb618feaf15c13c140d66c04a761511cc803103
Shakirudeen Lasisi <s.lasisi@waam3d.com>
2025-07-27 18:39:23 +0100

    add lesson files

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            &#169; 2025 Bobo development. All rights reserved.
+        </footer>
+    )
+}
---
+++ src/Page.jsx
@@ -0,0 +1,13 @@
+import "./Page.css"
+import Header from "./Components/Header";
+import { MainContent } from "./Components/MainContent";
+function Page() {
+    return (
+        <>
+            <Header/>
+            <MainContent/>
+        </>
+    );
+}
+
+export default Page;
