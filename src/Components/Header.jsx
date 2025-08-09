4726018cf66e98e700e93db69df45c54ebdab711
Shakirudeen Lasisi <s.lasisi@waam3d.com>
2025-08-09 11:32:15 +0100

    add changes

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import claudeIcon from "../assets/claude.png"
+export default function Header() {
+    return (
+        <header>
+            <img src={claudeIcon} alt="Chef Claude"/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
---
+++ src/Components/Main.jsx
@@ -0,0 +1,24 @@
+export default function Main() {
+    const data = []
+    var dataElements = data.map(x => <li key={x}> {x}</li>)
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const ingredient = formData.get("ingredient")
+        data.push(ingredient)
+        console.log("Form Submitted:" + ingredient)
+    }
+    return (
+        <main>
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="e.g pepper" aria-label="Add Ingredient" name="ingredient"/>
+                <button type="submit">Add Ingredient</button>
+            </form>
+            <h2> Ingredients </h2>
+            <ul>
+                { dataElements }
+            </ul>
+            
+        </main>
+    )
+}
