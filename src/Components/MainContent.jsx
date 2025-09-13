4bb7a4a07bfd4b5012cde6c5e72ec2f05bf5b51f
Shakirudeen Lasisi <s.lasisi@waam3d.com>
2025-09-13 10:15:32 +0100

    rewrite ingredient form to use uncontrolled components

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -3,28 +3,23 @@ export default function MainContent() {
     const [data, setData] = useState([])
     const dataElements = data.map(x =>
         <li key={x}> {x}</li>)
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
         const ingredient = formData.get("ingredient")
 
-        if (!ingredient) { return }
-        setData(prev => [...prev, ingredient])
-        
-        var ingredFormm = document.getElementById("ingredientInput")
-        ingredFormm.value = ""
+        if (!ingredient || ingredient.trim().length == 0) { return }
+        setData(prev => [...prev, ingredient.trim()])
+
         console.log("Form Submitted:" + ingredient)
     }
     return (
         <main>
-            <form className="add-ingredient-form" onSubmit={handleSubmit}>
-                <input id="ingredientInput" type="text" placeholder="e.g pepper" aria-label="Add Ingredient" name="ingredient"/>
-                <button type="submit">Add Ingredient</button>
+            <form className="add-ingredient-form" action={addIngredient}>
+                <input type="text" placeholder="e.g pepper" aria-label="Add Ingredient" name="ingredient"/>
+                <button>Add Ingredient</button>
             </form>
             <h2> Ingredients </h2>
             <ul>
-                { 
-                    dataElements }
+                { dataElements }
             </ul>
             
         </main>
